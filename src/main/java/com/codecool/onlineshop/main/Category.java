8e0010d1bfb137e3339f50fb1896763c4316d416
Wojciech Charuza <wojciechcharuza@MacBook-Pro-Wojciech-3.local>
2019-02-05 13:37:54 +0100

    implement category model

---
+++ src/main/java/com/codecool/onlineshop/main/Category.java
@@ -0,0 +1,41 @@
+package com.codecool.onlineshop.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+
+    private int id;
+    private String name;
+    private List<Product> products;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.products = new ArrayList<Product>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void populateProducts() {
+        
+    }
+}
