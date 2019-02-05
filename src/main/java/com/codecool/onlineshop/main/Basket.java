34f39c9f48cd32e1f369deccd57afd6c6b96217a
Wojciech Charuza <wojciechcharuza@MacBook-Pro-Wojciech-3.local>
2019-02-05 14:04:21 +0100

    Implement Basket class

---
+++ src/main/java/com/codecool/onlineshop/main/Basket.java
@@ -0,0 +1,31 @@
+package com.codecool.onlineshop.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket {
+
+    private int id;
+    private List<Product> products;
+
+    public Basket(int id) {
+        this.products = new ArrayList<Product>();
+        this.id = id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void deleteProduct(Product product) {
+        products.remove(product);
+    }
+}
