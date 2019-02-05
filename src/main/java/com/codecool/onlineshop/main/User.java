cff3d173d4c4647d1f61fa5d96aa660381bda98f
Kamil Bracki <kamilbracki1@gmail.com>
2019-02-05 14:56:36 +0100

    add User

---
+++ src/main/java/com/codecool/onlineshop/main/User.java
@@ -0,0 +1,48 @@
+package com.codecool.onlineshop.main;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String password;
+    private String userType;
+
+    public User (Integer id, String name, String password, String userType){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
