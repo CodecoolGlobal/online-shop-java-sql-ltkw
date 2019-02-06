8422f38ee0299e8697297c7e09fc62a295937502
Tatiana Slonimskaia <slonimskaia@gmail.com>
2019-02-06 11:50:49 +0100

    implementation of ProductsDaoImpl

--- src/main/java/com/codecool/onlineshop/main/Main.java
+++ src/main/java/com/codecool/onlineshop/main/Main.java
@@ -1,7 +1,11 @@
 package com.codecool.onlineshop.main;
 
+import com.codecool.onlineshop.dao.ProductsDaoImpl;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ProductsDaoImpl product = new ProductsDaoImpl();
+        product.getProductData();
     }
 }
