05857e68e6f0b0bac421bddf1a6689af2ce9f05b
Wojciech Charuza <wojciechcharuza@MacBook-Pro-Wojciech-3.local>
2019-02-06 13:49:50 +0100

    Init customer controller

--- src/main/java/com/codecool/onlineshop/service/CustomerService.java
+++ src/main/java/com/codecool/onlineshop/service/CustomerService.java
@@ -7,24 +7,35 @@ import com.codecool.onlineshop.model.User;
 
 public class CustomerService {
 
-    User user;
+    private User user;
+    private Iterator<Product> iterator;
 
     public CustomerService(User user) {
         this.user = user;
+        this.iterator = user.getBasket().getIterator();
     }
 
     // public void addProductToBasket(Product product, int amount) {
     // user.getBasket().addProduct(product, amount);
     // }
 
-    public void removeProductFromBasket(Product product) {
-        user.getBasket().deleteProduct(product);
+    public void removeProductFromBasket(int id) {
+        if (iterator.hasNext()) {
+            if (iterator.next().getId() == id) {
+                user.getBasket().deleteProduct(iterator.next());
+            }
+        }
     }
 
     public void DisplayAllProducts() {
-        Iterator<Product> iterator = user.getBasket().getIterator();
         if (iterator.hasNext()) {
             iterator.next().toString();
         }
     }
+
+    public User getUser() {
+        return this.user;
+    }
+
+
 }
