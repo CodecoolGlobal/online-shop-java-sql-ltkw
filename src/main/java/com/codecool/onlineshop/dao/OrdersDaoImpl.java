c35c8474c6b0cb615a0cf9153dc4e06ecd52c5bf
Liz <pachaffy@gmail.com>
2019-02-07 17:28:31 +0100

    Delete unneccessary imports in OrdersDaoImpl

--- src/main/java/com/codecool/onlineshop/dao/OrdersDaoImpl.java
+++ src/main/java/com/codecool/onlineshop/dao/OrdersDaoImpl.java
@@ -1,8 +1,6 @@
 package com.codecool.onlineshop.dao;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
