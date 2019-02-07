629f33e8094e60b8b7fe1241248f750dfa3a6af6
Liz <pachaffy@gmail.com>
2019-02-07 17:27:40 +0100

    Delete unneccessary fields and methods

--- src/main/java/com/codecool/onlineshop/model/Order.java
+++ src/main/java/com/codecool/onlineshop/model/Order.java
@@ -3,12 +3,9 @@ package com.codecool.onlineshop.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.codecool.onlineshop.model.Basket;
-
 public class Order {
 
     private Integer orderId;
-    private Basket basket;
     private int userID;
     private OrderStatus orderStatus;
     private List<Order> orders;
@@ -16,19 +13,13 @@ public class Order {
 
     public Order(int orderId, int productId, int productAmount, int productAmountPrice, int userID) {
         this.orderId = orderId;
-        this.basket = basket;
         this.userID = userID;
         this.orderStatus = OrderStatus.SUBMIT;
-        this.orderSumPrice = orderSumPrice;
         orders = new ArrayList<>();
     }
 
     public int getOrderId() {
         return orderId;
-    }
-
-    public Basket getBasket() {
-        return basket;
     }
 
     public int getUserID() {
@@ -41,10 +32,6 @@ public class Order {
 
     public void setOrderId(int orderId) {
         this.orderId = orderId;
-    }
-
-    public void setBasket(Basket basket) {
-        this.basket = basket;
     }
 
     public void setUser(int userID) {
