7d837cdc9df6527045fe4e9331225be668dbdde5
Liz <pachaffy@gmail.com>
2019-02-08 10:07:02 +0100

    Add data to Order

--- src/main/java/com/codecool/onlineshop/model/Order.java
+++ src/main/java/com/codecool/onlineshop/model/Order.java
@@ -8,14 +8,24 @@ public class Order {
     private String productName;
     private int productAmount;
     private int productAmountPrice;
+    private int totalPrice;
+    private String date;
 
-    public Order(int orderId, int productId, String productName, int productAmount, int productAmountPrice, int userID) {
+    public Order(int orderId, int productId, String productName, int productAmount, int productAmountPrice, int userID, String date) {
         this.orderId = orderId;
         this.userID = userID;
         this.productAmount = productAmount;
         this.productId = productId;
         this.productAmountPrice = productAmountPrice;
         this.productName = productName;
+        this.date = date;
+    }
+
+    public Order(int orderId, String date, int userId, int totalPrice) {
+        this.orderId = orderId;
+        this.userID = userId;
+        this.totalPrice = totalPrice;
+        this.date = date;
     }
 
     public int getProductAmountPrice() {
@@ -41,8 +51,17 @@ public class Order {
     public int getUserID() {
         return userID;
     }
+    
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public String getDate() {
+        return date;
+    }
 
     public int compareTo(Order other) {
         return this.orderId.compareTo(other.orderId);
     }
+
 }
