5d8820ff41d2bcc1dcc8985bb7533b55264f41ae
Tatiana Slonimskaia <slonimskaia@gmail.com>
2019-02-18 13:24:20 +0100

    Add empty classes for test

---
+++ src/test/com/codecool/onlineshop/controller/AdminControllerTest.java
@@ -0,0 +1,9 @@
+package com.codecool.onlineshop.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTest {
+
+}
---
+++ src/test/com/codecool/onlineshop/service/CustomerServiceTest.java
@@ -0,0 +1,7 @@
+package com.codecool.onlineshop.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+}
