a7e507db3e4a174b94b1110c5c6a56d65c284fa2
Wojciech Charuza <wojciechcharuza@MacBook-Pro-Wojciech-3.local>
2019-02-21 14:49:46 +0100

    add tests for basket, product, product iterator

--- src/test/com/codecool/onlineshop/model/ProductTest.java
+++ src/test/com/codecool/onlineshop/model/ProductTest.java
@@ -8,13 +8,13 @@ class ProductTest {
 
     @Test
     void productToString() {
-        Product product = new Product(1, "Water", "Soft Drink", 5, 10, 0, 0);
+        Product product = new Product.Builder().withId(1).withName("Water").withCategory("Soft Drink").withPrice(5).withAmount(10).build();
         assertEquals("id = 1, \nname = Water, \ncategory = Soft Drink, \nprice = 5, \namount = 10", product.toString());
     }
 
     @Test
     void calculateRating() {
-        Product product = new Product(1, "Water", "Soft Drink", 5, 10, 15, 3);
+        Product product = new Product.Builder().withId(1).withName("Water").withCategory("Soft Drink").withPrice(5).withAmount(10).withRating(15).withNumberOfRatings(3).build();
         assertEquals(5.0, product.calculateRating());
     }
 
--- src/test/com/codecool/onlineshop/service/CustomerServiceTest.java
+++ src/test/com/codecool/onlineshop/service/CustomerServiceTest.java
@@ -12,15 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerServiceTest {
 
-    User user = new User(1, "Test", "test", "CUSTOMER");
-    CustomerService customerService = new CustomerService(user);
-    List<Product> products = new ArrayList<>();
-    Product product = new Product(1, "Water", "Soft Drink", 5, 10, 0, 0);
+
 
     @Test
     void validateRatingTest() {
 
-        assertEquals(5, customerService.validateRating());
 
     }
 
